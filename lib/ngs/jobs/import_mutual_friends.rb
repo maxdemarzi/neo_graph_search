c9c1f58ff0ac05c5715bab50cf75b22f94e0dd26
Max De Marzi <maxdemarzi@hotmail.com>
2013-01-27 02:29:37 -0600

    make import mutual friends job more robust

--- lib/ngs/jobs/import_mutual_friends.rb
+++ lib/ngs/jobs/import_mutual_friends.rb
@@ -8,28 +8,32 @@ module Job
       if user
         friend = User.find_by_uid(person_id)
 
-        # Import mutual friends
-        mutual_friends = user.client.get_connections("me", "mutualfriends/#{person_id}")
+        if friend
+          # Import mutual friends
+          mutual_friends = user.client.get_connections("me", "mutualfriends/#{person_id}")
 
-        commands = []
+          commands = []
 
-        # Make them friends
-        mutual_friends.each do |mutual_friend|
-          uid = mutual_friend["id"]
+          # Make them friends
+          mutual_friends.each do |mutual_friend|
+            uid = mutual_friend["id"]
 
-          node = User.find_by_uid(uid)
-          unless node
-            person = user.client.get_object(uid)
-            node = User.create_from_facebook(person)
+            node = User.find_by_uid(uid)
+            unless node
+              person = user.client.get_object(uid)
+              node = User.create_from_facebook(person)
+            end
+
+            if node
+              commands << [:create_unique_relationship, "friends_index", "ids",  "#{uid}-#{person_id}", "friends", node.neo_id, friend.neo_id]
+              commands << [:create_unique_relationship, "friends_index", "ids",  "#{person_id}-#{uid}", "friends", friend.neo_id, node.neo_id]
+            end
           end
 
-          commands << [:create_unique_relationship, "friends_index", "ids",  "#{uid}-#{person_id}", "friends", node.neo_id, friend.neo_id]
-          commands << [:create_unique_relationship, "friends_index", "ids",  "#{person_id}-#{uid}", "friends", friend.neo_id, node.neo_id]
-
+          batch_result = $neo_server.batch *commands
         end
-
-        batch_result = $neo_server.batch *commands
       end
     end
+
   end
 end
